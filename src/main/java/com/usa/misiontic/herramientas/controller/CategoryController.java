88ee7d9761f4ee73c128445a775c379abd5598d1
EdicsonEdicson5 <edicson.rico.mt@usa.edu.co>
2022-10-10 21:50:31 -0500

    RETO 4

--- src/main/java/com/usa/misiontic/herramientas/controller/CategoryController.java
+++ src/main/java/com/usa/misiontic/herramientas/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.usa.misiontic.herramientas.controller;
 
 
+import com.usa.misiontic.herramientas.entities.Admin;
 import com.usa.misiontic.herramientas.entities.Category;
 import com.usa.misiontic.herramientas.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,23 +9,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/Category")
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
     @GetMapping("/all")
-    public List<Category> getAll(){
-        return categoryService.getAll();
+    public List<Category> getAll() { return categoryService.getAll();}
+
+    @GetMapping("/{id}")
+    public Optional<Category> getCategory(@PathVariable("id") int id){
+        return categoryService.getCategory(id);
     }
 
     @PostMapping("/save")
     @ResponseStatus(HttpStatus.CREATED)
-    public Category save(@RequestBody Category p){
-        return categoryService.save(p);
+    public Category save (@RequestBody Category a) {return categoryService.save(a);}
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category update (@RequestBody Category a){return categoryService.update(a);}
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int id){
+        return categoryService.delete(id);
     }
 
 }
